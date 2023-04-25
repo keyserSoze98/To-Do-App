9ff855755a2612211825c7006aea89d783cda779
Kishan Das(kishan.das) <kishan.das@infosys.com>
2023-04-25 15:11:40 +0530

    push successful

---
+++ app/src/main/java/com/keysersoze/todoapp/OnDialogCloseListener.java
@@ -0,0 +1,9 @@
+package com.keysersoze.todoapp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+
+    void onDialogCLose(DialogInterface dialogInterface);
+
+}
